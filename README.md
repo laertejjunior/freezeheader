660344c73ecf1812ee7e248404ad5b8fbee668c9
laertejjunior <laertejjunior@gmail.com>
2013-04-13 23:25:37 -0300

    start project

---
+++ README.md
@@ -0,0 +1,55 @@
+<article class="markdown-body entry-content" itemprop="mainContentOfPage"><h1>
+<a name="jqueryfreezeheader-" class="anchor" href="#jquerycookie-"><span class="mini-icon mini-icon-link"></span></a>
+jquery.freezeheader 
+</h1>
+
+<p> A simple jquery plugin to freeze header row in html table.</p>
+
+<h2>
+<a name="installation" class="anchor" href="#installation"><span class="mini-icon mini-icon-link"></span></a>Installation</h2>
+
+<p>Include script <em>after</em> the jQuery library (unless you are packaging scripts somehow else):</p>
+
+<pre><code>&lt;script src="/path/to/jquery.freezeheader.js"&gt;&lt;/script&gt;
+</code></pre>
+
+<p><strong>Do not include the script directly from GitHub (<a href="http://raw.github.com/...">http://raw.github.com/...</a>).</strong> The file is being served as text/plain and as such being blocked
+in Internet Explorer on Windows 7 for instance (because of the wrong MIME type). Bottom line: GitHub is not a CDN.</p>
+
+
+<h2>
+<a name="usage" class="anchor" href="#usage"><span class="mini-icon mini-icon-link"></span></a>Usage</h2>
+
+<p>Create a table with fixed header in the top browser:</p>
+
+```javascript
+        $(document).ready(function () {
+            $("#tableid").freezeHeader();
+        })
+```
+
+<p>Create a table with fixed header and scroll bar:</p>
+
+```javascript
+        $(document).ready(function () {
+            $("#tableid").freezeHeader({ 'height': '300px' });
+        })
+```
+
+<h2>
+<a name="demo" class="anchor" href="#demo"><span class="mini-icon mini-icon-link"></span></a>Demo</h2>
+
+<h2>
+<a name="development" class="anchor" href="#development"><span class="mini-icon mini-icon-link"></span></a>Development</h2>
+
+<ul>
+<li>Source hosted at <a href="https://github.com/laertejjunior">GitHub</a>
+</li>
+<li>Report issues, questions, feature requests on <a href="https://github.com/laertejjunior/issues">GitHub Issues</a>
+</li>
+</ul><p>Pull requests are very welcome! Make sure your patches are well tested. Please create a topic branch for every separate change you make.</p>
+
+<h2>
+<a name="authors" class="anchor" href="#authors"><span class="mini-icon mini-icon-link"></span></a>Authors</h2>
+
+<p><a href="https://github.com/laertejjunior">Laerte Mercier Junior</a></p></article>
---
+++ js/jquery.freezeheader.js
@@ -0,0 +1,118 @@
+/* ------------------------------------------------------------------------
+Class: freezeHeader
+Use:freeze header row in html table
+Example:  $('#tableid').freezeHeader();
+Author: Laerte Mercier Junior
+Version: 1.0.1
+-------------------------------------------------------------------------*/
+(function ($) {
+    $.fn.freezeHeader = function (params) {
+        var copiedHeader = false;
+        var idObj = this.selector.replace('#', '');
+        var container;
+        var grid;
+        var conteudoHeader;
+        var openDivScroll = '';
+        var closeDivScroll = '';
+         
+
+        if (params && params.height !== undefined) {
+            divScroll = '<div id="hdScroll' + idObj + '" style="height: ' + params.height + '; overflow-y: scroll">';
+            closeDivScroll = '</div>';
+        }
+        
+        grid = $('table[id$="' + idObj + '"]');
+        conteudoHeader = grid.find('thead');
+
+		
+
+
+    
+            if (params && params.height !== undefined) {
+                if ($('#hdScroll' + idObj).length == 0) {
+                    grid.wrapAll(divScroll);
+                }
+            }
+
+
+            var obj = params && params.height !== undefined
+               ? $('#hdScroll' + idObj)
+               : $(window);
+
+
+            if ($('#hd' + idObj).length == 0) {
+                grid.before('<div id="hd' + idObj + '"></div>');
+        }
+
+        obj.scroll(function () { freezeHeader(); })
+
+        function freezeHeader() {
+           
+            if ($('table[id$="' + idObj + '"]').length > 0) {
+
+
+      
+                container = $('#hd' + idObj);
+			//	console.log(conteudoHeader.offset());
+                if (conteudoHeader.offset() != null) {
+                    if (limiteAlcancado(params)) {
+                        
+						if (!copiedHeader) {
+                            cloneHeaderRow(grid);
+                            copiedHeader = true;
+                        }
+                    }
+                    else {
+                        container.css("visibility", "hidden");
+                        container.css("top", "0px");
+                        container.width(0);
+                        copiedHeader = false;
+                    }
+                }
+            }
+        }
+
+        function limiteAlcancado(params) {
+			
+            if (params && params.height !== undefined) {
+                return (conteudoHeader.offset().top <= obj.offset().top);
+            }
+            else {
+                return ($(document).scrollTop() > conteudoHeader.offset().top && $(document).scrollTop() < grid.height() + conteudoHeader.offset().top);
+            }
+        }
+
+        function cloneHeaderRow() {
+		    container.html('');
+            container.val('');
+            var tabela = $('<table style="margin: 0 0;"></table>');
+            var atributos = grid.prop("attributes");
+
+            $.each(atributos, function () {
+                console.log($().jquery);
+				if (this.name != "id")
+				{
+                    tabela.attr(this.name, this.value);
+					}
+            });
+			
+            tabela.append('<thead>' + conteudoHeader.html() + '</thead>');
+           
+		   container.append(tabela);
+            container.width(conteudoHeader.outerWidth());
+            container.height(conteudoHeader.height);
+            container.find('th').each(function (index) {
+                var cellWidth = grid.find('th').eq(index).outerWidth();
+                $(this).css('width', cellWidth);
+            });
+
+            container.css("visibility", "visible");
+
+            if (params && params.height !== undefined) {
+                container.css("top", obj.offset().top + "px");
+                container.css("position", "absolute");
+            } else
+                container.css("position", "fixed");
+        }
+    };
+})(jQuery);
