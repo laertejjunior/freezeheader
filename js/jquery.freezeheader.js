88f6a507216386ce187951b0fed3a65cbaf4326f
laertejjunior <laertejjunior@gmail.com>
2013-05-23 14:39:20 -0300

    adicionada nova forma de uso

--- js/jquery.freezeheader.js
+++ js/jquery.freezeheader.js
@@ -3,115 +3,128 @@ Class: freezeHeader
 Use:freeze header row in html table
 Example 1:  $('#tableid').freezeHeader();
 Example 2:  $("#tableid").freezeHeader({ 'height': '300px' });
+Example 3:  $("table").freezeHeader();
+Example 4:  $(".table2").freezeHeader();
 Author: Laerte Mercier Junior
-Version: 1.0.3
+Version: 1.0.4
 -------------------------------------------------------------------------*/
 (function ($) {
+    var TABLE_ID = 0;
     $.fn.freezeHeader = function (params) {
+
         var copiedHeader = false;
-        var idObj = this.selector.replace('#', '');
-        var container;
-        var grid;
-        var conteudoHeader;
-        var openDivScroll = '';
-        var closeDivScroll = '';
 
-        if (params && params.height !== undefined) {
-            divScroll = '<div id="hdScroll' + idObj + '" style="height: ' + params.height + '; overflow-y: scroll">';
-            closeDivScroll = '</div>';
-        }
+        function freezeHeader(elem) {
+            var idObj = elem.attr('id') || ('tbl-' + (++TABLE_ID));
+            if (elem.length > 0 && elem[0].tagName.toLowerCase() == "table") {
 
-        grid = $('table[id$="' + idObj + '"]');
-        conteudoHeader = grid.find('thead');
+                var obj = {
+                    id: idObj,
+                    grid: elem,
+                    container: null,
+                    header: null,
+                    divScroll: null,
+                    openDivScroll: null,
+                    closeDivScroll: null,
+                    scroller: null
+                };
 
-        if (params && params.height !== undefined) {
-            if ($('#hdScroll' + idObj).length == 0) {
-                grid.wrapAll(divScroll);
-            }
-        }
+                if (params && params.height !== undefined) {
+                    obj.divScroll = '<div id="hdScroll' + obj.id + '" style="height: ' + params.height + '; overflow-y: scroll">';
+                    obj.closeDivScroll = '</div>';
+                }
 
-        var obj = params && params.height !== undefined
-           ? $('#hdScroll' + idObj)
-           : $(window);
+                obj.header = obj.grid.find('thead');
 
-        if ($('#hd' + idObj).length == 0) {
-            grid.before('<div id="hd' + idObj + '"></div>');
-        }
-
-        obj.scroll(function () { freezeHeader(); })
-
-        function freezeHeader() {
-
-            if ($('table[id$="' + idObj + '"]').length > 0) {
-
-                container = $('#hd' + idObj);
-                if (conteudoHeader.offset() != null) {
-                    if (limiteAlcancado(params)) {
-                        if (!copiedHeader) {
-                            cloneHeaderRow(grid);
-                            copiedHeader = true;
-                        }
-                    }
-                    else {
-
-                        if (($(document).scrollTop() > conteudoHeader.offset().top)) {
-                            container.css("position", "absolute");
-                            container.css("top", (grid.find("tr:last").offset().top - conteudoHeader.height()) + "px");
-                        }
-                        else {
-                            container.css("visibility", "hidden");
-                            container.css("top", "0px");
-                            container.width(0);
-                        }
-
-                        copiedHeader = false;
-
+                if (params && params.height !== undefined) {
+                    if ($('#hdScroll' + obj.id).length == 0) {
+                        obj.grid.wrapAll(obj.divScroll);
                     }
                 }
+
+                obj.scroller = params && params.height !== undefined
+				   ? $('#hdScroll' + obj.id)
+				   : $(window);
+
+                obj.scroller.on('scroll', function () {
+
+                    if ($('#hd' + obj.id).length == 0) {
+                        obj.grid.before('<div id="hd' + obj.id + '"></div>');
+                    }
+
+                    obj.container = $('#hd' + obj.id);
+
+                    if (obj.header.offset() != null) {
+                        if (limiteAlcancado(obj, params)) {
+                            if (!copiedHeader) {
+                                cloneHeaderRow(obj);
+                                copiedHeader = true;
+                            }
+                        }
+                        else {
+
+                            if (($(document).scrollTop() > obj.header.offset().top)) {
+                                obj.container.css("position", "absolute");
+                                obj.container.css("top", (obj.grid.find("tr:last").offset().top - obj.header.height()) + "px");
+                            }
+                            else {
+                                obj.container.css("visibility", "hidden");
+                                obj.container.css("top", "0px");
+                                obj.container.width(0);
+                            }
+                            copiedHeader = false;
+                        }
+                    }
+
+                });
             }
         }
 
-        function limiteAlcancado(params) {
+        function limiteAlcancado(obj, params) {
             if (params && params.height !== undefined) {
-                return (conteudoHeader.offset().top <= obj.offset().top);
+                return (obj.header.offset().top <= obj.scroller.offset().top);
             }
             else {
-                return ($(document).scrollTop() > conteudoHeader.offset().top && $(document).scrollTop() < (grid.height() - conteudoHeader.height() - grid.find("tr:last").height()) + conteudoHeader.offset().top);
+                return ($(document).scrollTop() > obj.header.offset().top && $(document).scrollTop() < (obj.grid.height() - obj.header.height() - obj.grid.find("tr:last").height()) + obj.header.offset().top);
             }
         }
 
-        function cloneHeaderRow() {
-            container.html('');
-            container.val('');
+        function cloneHeaderRow(obj) {
+            obj.container.html('');
+            obj.container.val('');
             var tabela = $('<table style="margin: 0 0;"></table>');
-            var atributos = grid.prop("attributes");
+            var atributos = obj.grid.prop("attributes");
 
             $.each(atributos, function () {
-
                 if (this.name != "id") {
                     tabela.attr(this.name, this.value);
                 }
             });
 
-            tabela.append('<thead>' + conteudoHeader.html() + '</thead>');
+            tabela.append('<thead>' + obj.header.html() + '</thead>');
 
-            container.append(tabela);
-            container.width(conteudoHeader.width());
-            container.height(conteudoHeader.height);
-            container.find('th').each(function (index) {
-                var cellWidth = grid.find('th').eq(index).width();
+            obj.container.append(tabela);
+            obj.container.width(obj.header.width());
+            obj.container.height(obj.header.height);
+            obj.container.find('th').each(function (index) {
+                var cellWidth = obj.grid.find('th').eq(index).width();
                 $(this).css('width', cellWidth);
             });
 
-            container.css("visibility", "visible");
+            obj.container.css("visibility", "visible");
 
             if (params && params.height !== undefined) {
-                container.css("top", obj.offset().top + "px");
-                container.css("position", "absolute");
+                obj.container.css("top", obj.scroller.offset().top + "px");
+                obj.container.css("position", "absolute");
             } else {
-                container.css("top", "0px");
-                container.css("position", "fixed");
+                obj.container.css("top", "0px");
+                obj.container.css("position", "fixed");
             }
         }
+
+        return this.each(function (i, e) {
+            freezeHeader($(e));
+        });
+
     };
 })(jQuery);
