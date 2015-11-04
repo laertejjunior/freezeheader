58facb834d157c63402ad7ce5cebd2c5a63fd279
Laerte Mercier Junior <laertejjunior@gmail.com>
2015-11-04 13:52:37 -0200

    data used in example page

---
+++ javascripts/loadData.js
@@ -0,0 +1,8 @@
+function loadData() {
+	for ( i = 0; i < 100; i++) {
+		$('#table1 > tbody:last').append('<tr><td>hr-HR</td><td>Croatian - Croatia</td><td>0x041A</td><td>HRV</td></tr>');
+		$('#table2 > tbody:last').append('<tr><td>hr-HR</td><td>Croatian - Croatia</td><td>0x041A</td><td>HRV</td></tr>');
+		$('#table3 > tbody:last').append('<tr><td>hr-HR</td><td>Croatian - Croatia</td><td>0x041A</td><td>HRV</td></tr>');
+		$('#table4 > tbody:last').append('<tr><td>hr-HR</td><td>Croatian - Croatia</td><td>0x041A</td><td>HRV</td></tr>');
+	}
+}
